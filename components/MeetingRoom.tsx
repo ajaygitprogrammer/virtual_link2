c561bfaa06657dec52b42ef4dfefca013c38c916
gauravsharmaworld <gauravsharma102002@gmail.com>
2024-05-29 03:09:38 +0530

    first commit

---
+++ components/MeetingRoom.tsx
@@ -0,0 +1,115 @@
+'use Client';
+import { CallControls, CallParticipantsList, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk';
+import { Speaker } from 'lucide-react';
+import React from 'react';
+import { cn } from '@/lib/utils';
+import { useState } from 'react';
+import { LayoutList } from 'lucide-react';
+import { Users } from 'lucide-react';
+import { CallStatsButton } from '@stream-io/video-react-sdk';
+import { useSearchParams } from 'next/navigation';
+import EndCallButton from './EndCallButton';
+import { useCallStateHooks } from '@stream-io/video-react-sdk';
+import { CallingState } from '@stream-io/video-react-sdk';
+import Loader from './Loader';
+import { useRouter } from 'next/navigation';
+
+import{
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger}
+  from "@/components/ui/dropdown-menu"
+import { Client } from '@clerk/nextjs/server';
+
+
+
+type CallLayoutType='grid' | 'speaker-left' | 'speaker-right'
+
+const MeetingRoom = () => {
+  const searchParams=useSearchParams();
+  const isPersonalRoom=!!searchParams.get('personal')
+  const [layout,setLayout]=useState<CallLayoutType>('speaker-left')
+  const[showParticipants,setshowParticipants]=useState(false)
+  const router = useRouter();
+
+  const {useCallCallingState} = useCallStateHooks();
+  const callingState = useCallCallingState();
+
+  if(callingState !== CallingState.JOINED) return <Loader/>
+
+  const CallLayout=()=>
+    {
+      switch (layout)
+      {
+        case 'grid':
+          return <PaginatedGridLayout/>
+        case 'speaker-right':
+          return <SpeakerLayout participantsBarPosition='left'/>
+          default:
+          return <SpeakerLayout participantsBarPosition='right'/>
+
+       
+      }
+    }
+  return (
+   <section className='relative h-screen w-full overflow pt-4 text-white'>
+    <div className='relative flex size-full items-center justify-center'>
+      <div className='flex size-full max-w-[1000px] items-center'>
+        <CallLayout/>
+     </div>
+
+     <div className={cn('h-[calc(100vh-86px)] hidden ml-2',{'show-block':showParticipants})}>
+      <CallParticipantsList onClose={()=>setshowParticipants(false)}/>
+     </div>
+     <div className='fixed bottom-0 flex w-full items-center justify-center gap-5 flex-wrap'>
+      <CallControls onLeave={()=>
+        router.push('/')
+      }/>
+
+   <DropdownMenu>
+    <div className='flex items-center'>
+       <DropdownMenuTrigger
+       className='cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]'>
+        <LayoutList size={20}
+        className="text-white"/>
+       </DropdownMenuTrigger>
+    </div>
+      <DropdownMenuContent className='border-dark-1 bg-dark-1 text-white'>
+       {['grid','Speaker-Left','Speaker-Right'].map((item,index)=>
+      (
+        <div key={index}>
+          <DropdownMenuItem className='cursor-ointer'
+          onClick={()=>{
+            setLayout(item.toLowerCase() as CallLayoutType)
+          }}>
+
+            {item}
+          </DropdownMenuItem>
+          <DropdownMenuSeparator className='border-dark-1' />
+        </div>
+      ))}
+      
+      
+     </DropdownMenuContent>
+  </DropdownMenu>
+  <CallStatsButton/>
+  <button onClick={()=> setshowParticipants((prev)=>!prev)}>
+    <div className='cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]'>
+         <Users size={20} className="text-white"/>
+    </div>
+
+  </button>
+
+  {!isPersonalRoom && <EndCallButton/>}
+
+
+     </div>
+    </div>
+   </section>
+  )
+}
+
+export default MeetingRoom
---
+++ hooks/useGetCallByid.ts
@@ -0,0 +1,33 @@
+import { useState } from "react"
+import { Call } from "@stream-io/video-react-sdk"
+import { useStreamVideoClient } from "@stream-io/video-react-sdk"
+import { useEffect } from "react"
+export const useGetCallById=(id:string | string[])=>
+    {
+        const [call,setcall] = useState<Call>()
+        const[isCallLoading,setIsCallLoading]=useState(true)
+        const client=useStreamVideoClient();
+        useEffect(()=>
+            {
+              if(!client) return;
+
+              const loadCall=async()=>
+                {
+                  const{calls}=await client.queryCalls(
+                    {
+                        filter_conditions:
+                        {
+                            id
+                        }
+                    }
+                  )
+                  if(calls.length>0) setcall(calls[0]);
+
+                  setIsCallLoading(false)
+
+                }
+                loadCall();
+            },[client,id]);
+
+            return{call,isCallLoading};
+    }
